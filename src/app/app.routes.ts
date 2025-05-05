6afc3bbb9d2508f1c6a824c6079915910d9c069a
Christopher Giese <cgiese@assono.de>
2025-05-05 10:55:54 +0200

    adds typing animation gif for future chatbot implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from './api-calls/chat/chat.component';
-import { SampleComponent } from './api-calls/sample/sample.component';
 import { TestComponent } from './api-calls/test/test.component';
 
 export const routes: Routes = [
-  { path: 'test', component: TestComponent },
-  { path: 'chat', component: ChatComponent },
-  { path: 'sample', component: SampleComponent },
-  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+    { path: 'test', component: TestComponent },
+    { path: 'chat', component: ChatComponent },
+    { path: '', redirectTo: '/chat', pathMatch: 'full' },
 ];
